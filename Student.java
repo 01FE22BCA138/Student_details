3e35441faae0ca5272c537d986a77b02e2d52195
01fe22bca138 <01fe22bca138@kletech.ac.in>
2024-03-18 11:18:07 +0530

    code to display student details with course registered and marks

---
+++ Student.java
@@ -0,0 +1,24 @@
+class Student {
+    String name="Sukanya";
+    String program="BCA";
+    int semester=4;
+    
+    public void displayDetails()
+    {
+        System.out.println("Name:"+name);
+        System.out.println("Program:"+program);
+        System.out.println("Semester:"+semester);
+    }
+
+    public static void main(String[] args)
+    {
+        Student s=new Student();
+        s.displayDetails();
+        StudentCourse sc= new StudentCourse();
+        sc.displayCourseDetails();
+
+    }
+
+
+
+}
